08bc5f9251a138cf5faa04dd8d6def8bd9e84d57
parm530 <parmsahadeo530@gmail.com>
2016-08-31 13:13:23 -0400

    Added class Pirate

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,14 @@
 class Pirate
+
+  attr_accessor :name, :weight, :height
+  PIRATES = []
+
+  def initialize(name, weight, height)
+    @name, @weight, @height = name, weight, height
+    PIRATES << self
+  end
+
+  def self.all
+    PIRATES
+  end
 end
