afa332862eff060bf0c9f10e1ab5363fb830c1a4
parm530 <parmsahadeo530@gmail.com>
2016-08-31 13:13:47 -0400

    Added class Ship

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,19 @@
 class Ship
+
+  attr_accessor :name, :type, :booty
+  SHIPS = []
+
+  def initialize(name, type, booty)
+    @name, @type, @booty = name, type, booty
+    SHIPS << self
+  end
+
+  def self.all
+    SHIPS
+  end
+
+  def self.clear
+    SHIPS.clear
+  end
+
 end
